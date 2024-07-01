d3c6909c6d3df393b46433cf16e38c7197fb7c8b
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-01 17:35:22 +0200

    CDN e scaffolding

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+header("Content-type: application/json")
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- style -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>php-todolist-json</title>
+</head>
+<body>
+
+<main id="app">
+    <h1>Php-TodoList-Json</h1>
+</main>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="./js/app.js"></script>
+</body>
+</html>
